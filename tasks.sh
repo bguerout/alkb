56382d4b333c24a2e1cc687e9300719113750445
Benoît Guérout <bguerout@gmail.com>
2022-06-08 09:41:37 +0200

    Add tools.sh to build custom Arch kernel

---
+++ tasks.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+readonly TASK=${1:?"Please provide a task"}
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly PACKAGE_DIR="${SCRIPT_DIR}/linux"
+readonly PATCHES_DIR="${SCRIPT_DIR}/patches"
+
+function clone_kernel() {
+  cd "$SCRIPT_DIR"
+  asp update linux
+  [[ -d "${PACKAGE_DIR}" ]] || asp export linux
+  cd ..
+}
+
+function patch_files() {
+  patch "${PACKAGE_DIR}/config" <"${PATCHES_DIR}/config.patch"
+  patch "${PACKAGE_DIR}/PKGBUILD" <"${PATCHES_DIR}/PKGBUILD.patch"
+
+  cd "$PACKAGE_DIR"
+  local SHA256SUMS
+  SHA256SUMS=$(makepkg -g -f -p PKGBUILD)
+
+  SHA256SUMS="${SHA256SUMS}" envsubst '$SHA256SUMS' <PKGBUILD | tee PKGBUILD
+  updpkgsums
+  cd ..
+}
+
+function build_kernel() {
+  cd "$PACKAGE_DIR"
+  makepkg -s
+  cd ..
+}
+
+function install_packages() {
+  cd "$PACKAGE_DIR"
+  pacman -U *.tar.zst
+  cd ..
+}
+
+#Call task
+$TASK
